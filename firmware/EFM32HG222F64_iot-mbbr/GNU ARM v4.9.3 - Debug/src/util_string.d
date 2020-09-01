31cf1006ac3dd2c898af81f9b18273a992915b14
Brecht Van Eeckhoudt <brecht.vaneeckhoudt@hotmail.com>
2020-09-02 01:15:09 +0200

    Update gitignore

--- firmware/EFM32HG222F64_iot-mbbr/GNU ARM v4.9.3 - Debug/src/util_string.d
+++
@@ -1,4 +0,0 @@
-src/util_string.o: ../src/util_string.c \
- /home/brecht/SimplicityStudio/v4_workspace/EFM32HG222F64_iot-mbbr/inc/util_string.h
-
-/home/brecht/SimplicityStudio/v4_workspace/EFM32HG222F64_iot-mbbr/inc/util_string.h:
